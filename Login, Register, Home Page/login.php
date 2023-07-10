e4021901ef72d44d82d1bca4f8b7367e5b4d7533
TehLax <lrhontiveros@ccc.edu.ph>
2023-07-10 23:14:22 +0800

    Minor QoL Changes to Registration
    *Added an Automatic Redirect to Login after registering

--- Login, Register, Home Page/login.php
+++ Login, Register, Home Page/login.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
- if (isset($_SESSION["user"])) {
-    header("Location: almazan/Homepage.php");
-}
+//  if (isset($_SESSION["user"])) {
+//     header("Location: almazan/Homepage.php");
+// }
 ?>
 <!DOCTYPE html>
 <html lang="en">
