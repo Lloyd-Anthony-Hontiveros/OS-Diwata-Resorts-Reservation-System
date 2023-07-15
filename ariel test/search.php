61a9ad347f6ece14d3602894e07ffcdcfb27d29f
yel-x <132632053+yel-x@users.noreply.github.com>
2023-07-14 22:10:16 -1200

    Update search.php

--- ariel test/search.php
+++ ariel test/search.php
@@ -1,48 +1,47 @@
 <?php
-include("connection/connect.php");
-$type = $_GET['type'];
+include("reservation.php");
+$type=$_GET['type'];
 
 
 ?>
-
 <head>
-	<meta charset="UTF-8">
-	<title>Villa Diwata Resort</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Linking Bootstrap css file -->
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<!-- Linking Main Css file -->
-	<link rel="stylesheet" type="text/css" href="css/animate.css">
-	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="css/flatpickr.min.css">
-	<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/color.css">
-	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+<meta charset="UTF-8">
+<title>Villa Diwata Resort</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Linking Bootstrap css file -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<!-- Linking Main Css file -->
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/flatpickr.min.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/color.css">
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 
 
 <body>
 
-	<div class="wrapper">
+<div class="wrapper">
 		<header class="abs">
 			<div class="top-bar">
 				<div class="container">
 					<div class="con-links">
 						<ul>
-							<li><i class="fa fa-phone" aria-hidden="true"></i> (+63)9636342280</li>
+								<li><i class="fa fa-phone" aria-hidden="true"></i> (+63)9636342280</li>
 							<li><i class="fa fa-envelope" aria-hidden="true"></i>ACL@gmail.com</li>
 						</ul>
 					</div><!--con-links end-->
 					<div class="social-links">
 						<ul class="social-lnks">
-							<li><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
-							<li><a href="#" title=""><i class="fa fa-twitter"></i></a></li>
-							<li><a href="#" title=""><i class="fa fa-google-plus"></i></a></li>
-
+							<li><a href="#" title=""><i class="fa-brands fa-twitter"></i></a></li>
+							<li><a href="#" title=""><i class="fa-brands fa-facebook"></i></a></li>
+							<li><a href="#" title=""><i class="fa-brands fa-instagram"></i></a></li>
 						</ul>
-
+						
 					</div><!--socail-links end-->
 				</div>
 			</div><!--top-bar end-->
@@ -57,17 +56,17 @@ $type = $_GET['type'];
 						<div class="menu-search">
 							<nav>
 								<ul>
-									<li class="active menu-item-has-children"><a href="index.php" title=""><i
-												class="fa fa-joomla"></i>Home</a>
-
+									<li class="active menu-item-has-children"><a href="index.php" title=""><i class="fa-solid fa-house"></i>Home</a>
+										
 									</li>
-
+									
 									<li><a href="about.php" title="">About Us</a></li>
-									<li><a href="term.php" title="">Terms and Condition</a></li>
+										<li><a href="term.php" title="">Terms and Condition</a></li>
 									<li><a href="contact.php" title="">Contact Us</a></li>
+									<li><a href="reservation.php" title="">Reservation</a></li>
 								</ul>
-							</nav><!--nav end-->
-
+		 					</nav><!--nav end-->
+							
 						</div><!--menu-search end-->
 						<div class="menu-icon">
 							<span class="first-bar"></span>
@@ -82,7 +81,7 @@ $type = $_GET['type'];
 
 
 
-		<div class="about-bg">
+<div class="about-bg">
 			<div class="container">
 				<div class="rl-banner">
 					<h2>Rooms List View</h2>
@@ -93,75 +92,70 @@ $type = $_GET['type'];
 				</div>
 			</div>
 		</div>
-
+	
 		<section>
 			<div class="block">
 				<div class="container">
 					<div class="row">
 						<div class="room-listing-style3">
+						
+						
+					
 
+<?php  
 
+	
+	
+	
 
+		// 			$mql = "SELECT * FROM booking where rtype='$type'";
+					
+		// 			$res= mysqli_query($db, $mql);
+		// while($row = mysqli_fetch_array($res))
+		// 		{  
+		// 	            $r_id =  $row['r_id'];
+		// 				$rimage =  $row['rimage'];
+		// 				$rtype =  $row['rtype'];
+		// 		        $rprice =  $row['rprice'];
+		// 				$rtext =  $row['rtext'];
+						
+					
+					
+		// 		echo '<div class="col-md-3">';
+		// 					echo 		'<div class="room-list-view">';
+		// 					echo 			'<figure>';
+		// 					echo 			"	<img src='admin/upload/".$rimage." '>";
+		// 					echo 			'</figure>';
+		// 						echo 		'<div class="room-info style2 style3">';
+		// 						echo 			"<h3>".$rtype."</h3>";
+		// 						echo 			"<p>".$rtext."</p>";
+		// 						echo 			'<ul class="feature-dt">';
+								
+		// 						echo 			'</ul>';
+		// 					echo 				'<div class="room-price">';
+		// 						echo 				"<h5>₱ ".$rprice."</h5>";
+		// 					echo 					'<span>Per Night</span>';
+		// 					echo 				'</div>';
+		// 					echo 				'<a href=booking.php?book='.$r_id.'  class="booking3">Book Now</a>';
+		// 					echo 			'</div>';
+		// 					echo 		'</div>';
+		// 					echo 	'</div>';
+	  
+	
+	
+					
+			
+	
+		// 		}
+					  
+					 
+				
+				
+				
+	
+    
 
-							<?php
-
-
-
-
-
-							$mql = "SELECT * FROM booking where rtype='$type'";
-
-							$res = mysqli_query($db, $mql);
-							while ($row = mysqli_fetch_array($res)) {
-								$r_id = $row['r_id'];
-								$rimage = $row['rimage'];
-								$rtype = $row['rtype'];
-								$rprice = $row['rprice'];
-								$rtext = $row['rtext'];
-
-
-
-								echo '<div class="col-md-3">';
-								echo '<div class="room-list-view">';
-								echo '<figure>';
-								echo "	<img src='admin/upload/" . $rimage . " '>";
-								echo '</figure>';
-								echo '<div class="room-info style2 style3">';
-								echo "<h3>" . $rtype . "</h3>";
-								echo "<p>" . $rtext . "</p>";
-								echo '<ul class="feature-dt">';
-
-								echo '</ul>';
-								echo '<div class="room-price">';
-								echo "<h5>₱ " . $rprice . "</h5>";
-								echo '<span>Per Night</span>';
-								echo '</div>';
-								echo '<a href=booking.php?book=' . $r_id . '  class="booking3">Book Now</a>';
-								echo '</div>';
-								echo '</div>';
-								echo '</div>';
-
-
-
-
-
-
-							}
-
-
-
-
-
-
-
-
-							?>
-
-
-
-
-
-
+// ?>				
 
 
 
@@ -175,18 +169,24 @@ $type = $_GET['type'];
 
 
 
-
-
-
-
-
-
-
-
-
+					
+							
+							
+							
+							
+							
+							
+						
+							
+							
+							
+					
+							
+							
+						
 						</div><!--room-listing-style3 end-->
 					</div>
-
+					
 				</div>
 			</div>
 		</section>
@@ -201,14 +201,12 @@ $type = $_GET['type'];
 									<div class="widget">
 										<div class="about-widget">
 											<a href="#" title=""><img src="images/icon logo.jpg" alt=""></a>
-											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-												nonummy nibh euismod it tincidunt ut laoreet commodo consequat. </p>
-
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,  sed diam nonummy nibh euismod it tincidunt ut laoreet commodo consequat.  </p>
+										
 											<ul class="sc-links">
-												<li><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
-												<li><a href="#" title=""><i class="fa fa-twitter"></i></a></li>
-												<li><a href="#" title=""><i class="fa fa-google-plus"></i></a></li>
-
+											<li><a href="#" title=""><i class="fa-brands fa-twitter"></i></a></li>
+											<li><a href="#" title=""><i class="fa-brands fa-facebook"></i></a></li>
+											<li><a href="#" title=""><i class="fa-brands fa-instagram"></i></a></li>
 											</ul>
 										</div><!--about-widget end-->
 									</div><!--widget end-->
@@ -297,8 +295,7 @@ $type = $_GET['type'];
 								<p>THANKYOU EVERYONE</p>
 							</div><!--copyright end-->
 							<div class="credit">
-								<p>Project of: <a href="#" title>Ariel Nazareno, Christian Almazan, LLoyd Hontiveros
-									</a></p>
+								<p>Project of: <a href="#" title>Ariel Nazareno, Christian Almazan, LLoyd Hontiveros </a></p>
 							</div><!--credit end-->
 						</div><!--bottom-footer end-->
 					</div>
@@ -312,24 +309,24 @@ $type = $_GET['type'];
 
 	</div><!--wrapper end-->
 
-	<!-- Including Jquery Js File -->
-	<script type="text/javascript" src="http://creativethemes.us/relax/js/jquery.min.js"></script>
-	<!-- Including Bootstrap js file -->
-	<script type="text/javascript" src="http://creativethemes.us/relax/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="http://creativethemes.us/relax/js/html5lightbox.js"></script>
-	<!-- Custom Js file -->
-	<script type="text/javascript" src="http://creativethemes.us/relax/js/script.js"></script>
+<!-- Including Jquery Js File -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/jquery.min.js"></script>
+<!-- Including Bootstrap js file -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="http://creativethemes.us/relax/js/html5lightbox.js"></script>
+<!-- Custom Js file -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/script.js"></script>
 
-	<!-- Including Jquery Js File -->
-	<script type="text/javascript" src="js/jquery.min.js"></script>
-	<!-- Including Bootstrap js file -->
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
-	<script type="text/javascript" src="js/flatpickr.min.js"></script>
-	<script type="text/javascript" src="js/isotope.js"></script>
-	<script type="text/javascript" src="js/html5lightbox.js"></script>
-	<script type="text/javascript" src="js/wow.js"></script>
+<!-- Including Jquery Js File -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<!-- Including Bootstrap js file -->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="js/flatpickr.min.js"></script>
+<script type="text/javascript" src="js/isotope.js"></script>
+<script type="text/javascript" src="js/html5lightbox.js"></script>
+<script type="text/javascript" src="js/wow.js"></script>
 
-	<!-- Custom Js file -->
-	<script type="text/javascript" src="js/script.js"></script>
+<!-- Custom Js file -->
+<script type="text/javascript" src="js/script.js"></script>
 </body>
