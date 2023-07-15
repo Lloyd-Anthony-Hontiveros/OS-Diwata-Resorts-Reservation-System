134d943baca26949e81ede79f57526537f6ebd60
yel-x <132632053+yel-x@users.noreply.github.com>
2023-07-14 22:11:00 -1200

    Update term.php

--- ariel test/term.php
+++ ariel test/term.php
@@ -1,23 +1,26 @@
+
+
 <!DOCTYPE html>
 <html>
 
 
 <head>
-	<meta charset="UTF-8">
-	<title>Villa Diwata Resort</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Linking Bootstrap css file -->
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<!-- Linking Main Css file -->
-	<link rel="stylesheet" type="text/css" href="css/animate.css">
-	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="css/flatpickr.min.css">
-	<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/color.css">
-	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+<meta charset="UTF-8">
+<title>Villa Diwata Resort</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Linking Bootstrap css file -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<!-- Linking Main Css file -->
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/flatpickr.min.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/color.css">
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-
+	
 	<link rel="stylesheet" type="text/css" href="css/popup.php">
 </head>
 
@@ -27,23 +30,24 @@
 	<div class="wrapper">
 
 
-		<header class="abs">
+	<header class="abs">
 			<div class="top-bar">
 				<div class="container">
 					<div class="con-links">
 						<ul>
-							<li><i class="fa fa-phone" aria-hidden="true"></i> (+63)9636342280</li>
+								<li><i class="fa fa-phone" aria-hidden="true"></i> (+63)9636342280</li>
 							<li><i class="fa fa-envelope" aria-hidden="true"></i>ACL@gmail.com</li>
 						</ul>
 					</div><!--con-links end-->
 					<div class="social-links">
 						<ul class="social-lnks">
-							<li><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
-							<li><a href="#" title=""><i class="fa fa-twitter"></i></a></li>
-							<li><a href="#" title=""><i class="fa fa-google-plus"></i></a></li>
-
+						<li><a href="#" title=""><i class="fa-brands fa-twitter"></i></a></li>
+						<li><a href="#" title=""><i class="fa-brands fa-facebook"></i></a></li>
+						<li><a href="#" title=""><i class="fa-brands fa-instagram"></i></a></li>
+						
+						
 						</ul>
-
+						
 					</div><!--socail-links end-->
 				</div>
 			</div><!--top-bar end-->
@@ -58,17 +62,18 @@
 						<div class="menu-search">
 							<nav>
 								<ul>
-									<li class="active menu-item-has-children"><a href="index.php" title=""><i
-												class="fa fa-joomla"></i>Home</a>
-
+									<li class="active menu-item-has-children"><a href="index.php" title=""><i class="fa-solid fa-house"></i>Home</a>
+										
 									</li>
-
+									
 									<li><a href="about.php" title="">About Us</a></li>
-									<li><a href="term.php" title="">Terms and Condition</a></li>
+										<li><a href="term.php" title="">Terms and Condition</a></li>
 									<li><a href="contact.php" title="">Contact Us</a></li>
-								</ul>
-							</nav><!--nav end-->
+									<li><a href="reservation.php" title="">Reservation</a></li>
 
+								</ul>
+		 					</nav><!--nav end-->
+							
 						</div><!--menu-search end-->
 						<div class="menu-icon">
 							<span class="first-bar"></span>
@@ -79,9 +84,9 @@
 				</div>
 			</div><!--bottom-header end-->
 		</header><!--Header end-->
-
-
-		<div class="about-bg">
+	
+	
+<div class="about-bg">
 			<div class="container">
 				<div class="rl-banner">
 					<h2>Terms & Conditions</h2>
@@ -99,56 +104,29 @@
 				<div class="container nopddd">
 					<div class="term-one">
 						<h3 class="term-title">What is this 'Lorem Ipsum' or 'Lorum Ipsum' stuff?</h3>
-						<p>In publishing and graphic design, placeholder text is commonly used to demonstrate the
-							elements of a document or visual presentation, such as font, typography, and layout. Even
-							though using "lorem ipsum" often arouses curiosity because of its resemblance to classical
-							Latin, it is not intended to have meaning. Where text is visible in a document, people tend
-							to focus on the textual content rather than upon overall presentation, so publishers use
-							lorem ipsum when displaying a typeface or design elements and page layout in order to direct
-							the focus to the publication style and not the meaning of the text.</p>
+						<p>In publishing and graphic design, placeholder text is commonly used to demonstrate the elements of a document or visual presentation, such as font, typography, and layout. Even though using "lorem ipsum" often arouses curiosity because of its resemblance to classical Latin, it is not intended to have meaning. Where text is visible in a document, people tend to focus on the textual content rather than upon overall presentation, so publishers use lorem ipsum when displaying a typeface or design elements and page layout in order to direct the focus to the publication style and not the meaning of the text.</p>
 					</div><!--term-one end-->
 					<div class="row">
 						<div class="tm-conditions">
 							<div class="col-md-6">
 								<div class="tmc-one">
 									<h3 class="term-title">Use of cookies</h3>
-									<p>Customization: Most generators only output paragraphs of about the same length.
-										Sometimes you need lists, headings, long paragraphs, short paragraphs, etc. The
-										website/magazine that you're designing probably isn't going to be filled with
-										uniform paragraphs, so your placeholder text should reflect that.</p>
+									<p>Customization: Most generators only output paragraphs of about the same length. Sometimes you need lists, headings, long paragraphs, short paragraphs, etc. The website/magazine that you're designing probably isn't going to be filled with uniform paragraphs, so your placeholder text should reflect that.</p>
 								</div><!--tmc-one end-->
 								<div class="tmc-one">
 									<h3 class="term-title">You ask, we answer!</h3>
 									<div class="condi">
 										<span>General / Owenrship</span>
-										<p>Quos quidem tibi studiose et diligenter tractandos magnopere censeo. Nemo
-											nostrum istius generis asotos iucunde putat vivere. Hoc non est positum in
-											nostra actione. Atque his de rebus et splendida est eorum et illustris
-											oratio. Igitur neque stultorum quisquam beatus neque sapientium non beatus.
-											Quid enim possumus hoc agere divinius? Profectus in exilium Tubulus statim
-											nec respondere ausus; Et non ex maxima parte de tota iudicabis? Heri,
-											inquam, ludis commissis ex urbe profectus veni ad vesperum. A quibus propter
-											discendi cupiditatem videmus ultimas terras esse peragratas. Quid est
-											igitur, cur ita semper deum appellet Epicurus beatum et aeternum?</p>
-									</div><!--condi end-->
+										<p>Quos quidem tibi studiose et diligenter tractandos magnopere censeo. Nemo nostrum istius generis asotos iucunde putat vivere. Hoc non est positum in nostra actione. Atque his de rebus et splendida est eorum et illustris oratio. Igitur neque stultorum quisquam beatus neque sapientium non beatus. Quid enim possumus hoc agere divinius? Profectus in exilium Tubulus statim nec respondere ausus; Et non ex maxima parte de tota iudicabis? Heri, inquam, ludis commissis ex urbe profectus veni ad vesperum. A quibus propter discendi cupiditatem videmus ultimas terras esse peragratas. Quid est igitur, cur ita semper deum appellet Epicurus beatum et aeternum?</p>
+									</div><!--condi end-->	
 									<div class="condi">
 										<span>Hyatt Tradmarks</span>
-										<p>Igitur neque stultorum quisquam beatus neque sapientium non beatus. Quid enim
-											possumus hoc agere divinius? Profectus in exilium Tubulus statim nec
-											respondere ausus; Et non ex maxima parte de tota iudicabis? Heri, inquam,
-											ludis commissis ex urbe profectus veni ad vesperum. A quibus propter
-											discendi cupiditatem videmus ultimas terras esse peragratas. Quid est
-											igitur, cur ita semper deum appellet Epicurus beatum et aeternum? Quos
-											quidem tibi studiose et diligenter tractandos magnopere censeo. Nemo nostrum
-											istius generis asotos iucunde putat vivere. Hoc non est positum in nostra
-											actione. Atque his de rebus et splendida est eorum et illustris oratio. </p>
+										<p>Igitur neque stultorum quisquam beatus neque sapientium non beatus. Quid enim possumus hoc agere divinius? Profectus in exilium Tubulus statim nec respondere ausus; Et non ex maxima parte de tota iudicabis? Heri, inquam, ludis commissis ex urbe profectus veni ad vesperum. A quibus propter discendi cupiditatem videmus ultimas terras esse peragratas. Quid est igitur, cur ita semper deum appellet Epicurus beatum et aeternum? Quos quidem tibi studiose et diligenter tractandos magnopere censeo. Nemo nostrum istius generis asotos iucunde putat vivere. Hoc non est positum in nostra actione. Atque his de rebus et splendida est eorum et illustris oratio. </p>
 									</div><!--condi end-->
 									<div class="condi">
 										<span>Copyrights</span>
-										<p>Hoc non est positum in nostra actione. Atque his de rebus et splendida est
-											eorum et illustris oratio. Igitur neque stultorum quisquam beatus neque
-											sapientium non beatus. Quid enim possumus hoc agere divinius. </p>
-									</div><!--condi end-->
+										<p>Hoc non est positum in nostra actione. Atque his de rebus et splendida est eorum et illustris oratio. Igitur neque stultorum quisquam beatus neque sapientium non beatus. Quid enim possumus hoc agere divinius. </p>
+									</div><!--condi end-->	
 								</div><!--tmc-one end-->
 							</div>
 							<div class="col-md-6">
@@ -156,35 +134,19 @@
 									<h3 class="term-title">Use of this Site</h3>
 									<div class="condi">
 										<span>General</span>
-										<p>Septem autem illi non suo, sed populorum suffragio omnium nominati sunt.Duo
-											Reges: constructio interrete. Cum praesertim illa perdiscere ludus esset.
-											Remihi non aeque satisfacit, et quidem locis pluribus. Ita graviter et
-											severe voluptatem secrevit a bono. Memini vero, inquam; Igitur neque
-											stultorum quisquam beatus neque sapientium non beatus. Nihil ad rem! Ne sit
-											sane; Quae quo sunt excelsiores, eo dant clariora indicia naturae.</p>
-									</div><!--condi end-->
+										<p>Septem autem illi non suo, sed populorum suffragio omnium nominati sunt.Duo Reges: constructio interrete. Cum praesertim illa perdiscere ludus esset. Remihi non aeque satisfacit, et quidem locis pluribus. Ita graviter et severe voluptatem secrevit a bono. Memini vero, inquam; Igitur neque stultorum quisquam beatus neque sapientium non beatus. Nihil ad rem! Ne sit sane; Quae quo sunt excelsiores, eo dant clariora indicia naturae.</p>
+									</div><!--condi end-->	
 									<div class="condi">
 										<span>Accounts / Passwords</span>
-										<p>Hoc non est positum in nostra actione. Atque his de rebus et splendida eorum
-											et illustris oratio. Igitur neque stultorum quisquam beatus neque sapientium
-											est non beatus. Quid enim possumus hoc agere divinius. </p>
+										<p>Hoc non est positum in nostra actione. Atque his de rebus et splendida eorum et illustris oratio. Igitur neque stultorum quisquam beatus neque sapientium est non beatus. Quid enim possumus hoc agere divinius. </p>
 									</div><!--condi end-->
 									<div class="condi">
 										<span>Reservations</span>
-										<p>Memini vero, inquam; Igitur neque stultorum quisquam beatus neque sapientium
-											non beatus. Nihil ad rem! Ne sit sane; Quae quo sunt excelsiores, eo dant
-											clariora indicia naturae. Septem autem illi non suo, sed populorum suffragio
-											omnium nominati sunt.Duo Reges: constructio interrete. Cum praesertim illa
-											perdiscere ludus esset. Re mihi non aeque satisfacit, et quidem locis
-											pluribus. Ita graviter et secrevit a bono. </p>
+										<p>Memini vero, inquam; Igitur neque stultorum quisquam beatus neque sapientium non beatus. Nihil ad rem! Ne sit sane; Quae quo sunt excelsiores, eo dant clariora indicia naturae. Septem autem illi non suo, sed populorum suffragio omnium nominati sunt.Duo Reges: constructio interrete. Cum praesertim illa perdiscere ludus esset. Re mihi non aeque satisfacit, et quidem locis pluribus. Ita graviter et secrevit a bono.  </p>
 									</div><!--condi end-->
 									<div class="condi">
 										<span>Noties and procedures for marking claims of copyright infringment</span>
-										<p>Cum praesertim illa perdiscere ludus esset. Re mihi aeque satisfacit, et
-											quidem locis pluribus. Ita graviter et secrevit a bono. Memini vero, inquam;
-											Igitur neque stultorum quisquam beatus neque sapientium non beatus. Nihil ad
-											Ne sit sane; Quae quo sunt excelsiores, eo dant clariora indicia naturae.
-											Septem autem suo, sed populorum suffragio omnium nominati. </p>
+										<p>Cum praesertim illa perdiscere ludus esset. Re mihi aeque satisfacit, et quidem locis pluribus. Ita graviter et secrevit a bono. Memini vero, inquam; Igitur neque stultorum quisquam beatus neque sapientium non beatus. Nihil ad Ne sit sane; Quae quo sunt excelsiores, eo dant clariora indicia naturae. Septem autem suo, sed populorum suffragio omnium nominati.  </p>
 									</div><!--condi end-->
 									<div class="condi">
 										<ul>
@@ -203,7 +165,7 @@
 			</div>
 		</section>
 
-
+	
 
 		<section>
 			<div class="block no-padding">
@@ -225,8 +187,8 @@
 				</div><!--newsletter end-->
 			</div>
 		</section>
-
-
+	
+	
 
 		<footer>
 			<div class="block no-padding">
@@ -238,14 +200,13 @@
 									<div class="widget">
 										<div class="about-widget">
 											<a href="#" title=""><img src="images/icon logo.jpg" alt=""></a>
-											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-												nonummy nibh euismod it tincidunt ut laoreet commodo consequat. </p>
-
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,  sed diam nonummy nibh euismod it tincidunt ut laoreet commodo consequat.  </p>
+										
 											<ul class="sc-links">
-												<li><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
-												<li><a href="#" title=""><i class="fa fa-twitter"></i></a></li>
-												<li><a href="#" title=""><i class="fa fa-google-plus"></i></a></li>
-
+											<li><a href="#" title=""><i class="fa-brands fa-twitter"></i></a></li>
+											<li><a href="#" title=""><i class="fa-brands fa-facebook"></i></a></li>
+											<li><a href="#" title=""><i class="fa-brands fa-instagram"></i></a></li>
+						
 											</ul>
 										</div><!--about-widget end-->
 									</div><!--widget end-->
@@ -334,8 +295,7 @@
 								<p>THANKYOU EVERYONE</p>
 							</div><!--copyright end-->
 							<div class="credit">
-								<p>Project of : <a href="#" title>Ariel Nazareno, Christian Almazan, LLoyd
-										Hontiveros</a></p>
+								<p>Project of : <a href="#" title>Ariel Nazareno, Christian Almazan, LLoyd Hontiveros</a></p>
 							</div><!--credit end-->
 						</div><!--bottom-footer end-->
 					</div>
@@ -349,17 +309,16 @@
 
 	</div><!--wrapper end-->
 
-	<!-- Including Jquery Js File -->
-	<script type="text/javascript" src="http://creativethemes.us/relax/js/jquery.min.js"></script>
-	<!-- Including Bootstrap js file -->
-	<script type="text/javascript" src="http://creativethemes.us/relax/js/bootstrap.min.js"></script>
-	<!-- Custom Js file -->
-	<script type="text/javascript" src="http://creativethemes.us/relax/js/script.js"></script>
+<!-- Including Jquery Js File -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/jquery.min.js"></script>
+<!-- Including Bootstrap js file -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/bootstrap.min.js"></script>
+<!-- Custom Js file -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/script.js"></script>
 
 
 </body>
 
 
 <!-- Mirrored from creativethemes.us/relax/reservation.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 21 Sep 2017 15:23:47 GMT -->
-
 </html>
